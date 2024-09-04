d2c66048337c75fa2875099c843c5c06c45edbaf
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-09-04 14:00:33 +0100

    shift items

--- src/requestsHandler/index.ts
+++ src/requestsHandler/index.ts
@@ -5,4 +5,3 @@ export const NETWORK = clusterApiUrl('mainnet-beta');
 
 // export const RPC = "https://mainnet.helius-rpc.com/?api-key=61c2ee69-b100-45d8-81e2-488dc6c4a5f0"
 
-export const RPC = "https://rpc.shyft.to?api_key=YJ4c0DNAEQK90NJn"
